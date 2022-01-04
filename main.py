4bf4fd4f6017c1cf8150ee33da32ba291a2ea4c5
alishaar <45029495+alishaar@users.noreply.github.com>
2022-01-04 11:23:26 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+import gc, os, sys, glob, argparse, utils
+import subprocess
+from openephys2nwb import convertOpenEphystoNWB
+import shutil
+
+
+if __name__ == "__main__":
+
+    p = argparse.ArgumentParser(description=\
+    'Convert open ephys directory to the Neurodata Without Borders file format')
+
+    p.add_argument('--source', type=str , dest = 'source_dir', help='source')
+    p.add_argument('--destination', type=str , dest = 'dest_dir', help='destination')
+
+    args = p.parse_args(); src = str(args.source_dir); dst = str(args.dest_dir)
+
+    convertOpenEphystoNWB(src, dst)
+
+
+
